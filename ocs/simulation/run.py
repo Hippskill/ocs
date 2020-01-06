Fix a bug in run_container and average cost calc

ocs/simulation/run.py
@@ -4,6 +4,8 @@ import docker
 import time
 import yaml
 
+import numpy as np
+
 from docker_utils import run_container
 
 
@@ -19,18 +21,21 @@ def run(config):
 
     print('start optimizing configuration for', workload['name'])
 
-    for n_cpu in range(1, int(simulation_limits['max_cpu']) + 1):
+    for n_cpu in range(4, int(simulation_limits['max_cpu']) + 1):
         # TODO(nmikhaylov): support ram
         for n_ram_gb in range(1, int(simulation_limits['max_ram_gb']) + 1):
-            cost = n_cpu * simulation_cost['cpu_core'] + n_ram_gb * simulation_cost['ram_gb']
-            print('try instance with cpu={} ram_gb={} cost={}'.format(n_cpu, n_ram_gb, cost))
+            cost_per_second = n_cpu * simulation_cost['cpu_core'] + n_ram_gb * simulation_cost['ram_gb']
+            print('try instance with cpu={} ram_gb={} cost_per_second={}'.format(n_cpu, n_ram_gb, cost_per_second))
             metrics = []
 
             for attempt in range(algrotihm['episodes']):
                 start_time = time.time()
 
                 # TODO(nmikhaylov): docker kill on ctrl+c
-                container_id = run_container(image=workload['image'], cpuset_cpus='1,2,3')
+                container_id = run_container(
+                    image=workload['image'],
+                    cpuset_cpus=','.join(map(str, range(1, n_cpu + 1)))
+                )
                 while True:
                     container = client.containers.get(container_id[:12])
                     if container.status != 'running':
@@ -44,8 +49,8 @@ def run(config):
 
                 print('attempt={} time elapsed: {}'.format(attempt, elapsed))
 
-            print('average metric', np.mean(metrics))
-            print('metric to cost', np.mean(metrics) / cost)
+            print('average time', np.mean(metrics))
+            print('average cost', np.mean(metrics) * cost)
 
 
 def main():