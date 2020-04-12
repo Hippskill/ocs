af74fedab4278182f969abff365458fb372ff7b8
Nikita Mikhaylov <nikita.mikhaylov@aimtech.com>
2020-04-12 10:56:38 +0300

    Add __str__ for instance_with_run_results

--- ocs/core/instance_with_run_results.py
+++ ocs/core/instance_with_run_results.py
@@ -11,3 +11,10 @@ class InstanceWithRunResults:
     def _parse_results(self):
         self.mean_cost = np.mean([run_result.cost for run_result in self._run_results])
         self.mean_elapsed = np.mean([run_result.elapsed_time for run_result in self._run_results])
+
+    def __str__(self):
+        return 'instance: {} mean_cost: {} mean_elapsed: {}'.format(
+            self.instance,
+            self.mean_cost,
+            self.mean_elapsed,
+        )
