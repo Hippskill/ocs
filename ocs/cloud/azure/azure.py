c0a1ff14ea8e5ae7f9fea50c05f8869332612fbb
Nikita Mikhaylov <nikita.mikhaylov@aimtech.com>
2020-04-18 18:08:11 +0300

    Parse pricing in azure cloud provider

--- ocs/cloud/azure/azure.py
+++ ocs/cloud/azure/azure.py
@@ -11,23 +11,50 @@ def az_vm():
     return az()['vm']
 
 
-def parse_instances(list_sizes_json):
+def parse_instances(pricing, list_sizes_json):
     available_instances = []
     for size in list_sizes_json:
-        print(size)
+        name = size['name']
+        cost_per_second = pricing.get_cost_per_second(name)
+
+        if cost_per_second is None:
+            print('skip {}, price not found'.format(name))
+            continue
+
         available_instances.append(Instance(
-            name=size['name'],
+            name=name,
             n_cpu=int(size['numberOfCores']),
             n_ram_gb=int(size['memoryInMb']) // 1024,
-            # TODO(nmikhaylov): find cost
-            cost_per_second=0.0
+            cost_per_second=float(cost_per_second) / 60 / 60
         ))
         print(str(available_instances[-1]))
     return available_instances
 
 
+class Pricing:
+
+    def __init__(self, pricing_filename):
+        self._price_by_name = {}
+
+        with open(pricing_filename) as f:
+            for line in f:
+                line = line.rstrip()
+                if len(line) == 0:
+                    continue
+
+                name, price = line.split()
+                self._price_by_name[name] = price
+
+    def get_cost_per_second(self, name):
+        name = str.lower(name)
+        if name in self._price_by_name:
+            return self._price_by_name[name]
+        return None
+
+
 class Azure:
-    def __init__(self):
+    def __init__(self, config):
+        self._pricing = Pricing(config['pricing'])
         self._available_instances = None
 
     def get_available_instances(self):
@@ -36,7 +63,7 @@ class Azure:
 
         #TODO(nmikhaylov): -l to config
         list_sizes_json = json.loads(az_vm()['list-sizes']['-l']['westus']())
-        self._available_instances = parse_instances(list_sizes_json)
+        self._available_instances = parse_instances(self._pricing, list_sizes_json)
         return self._available_instances
 
     def get_run_result(workload, instance):
