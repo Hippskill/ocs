fdfa0648b55eae2f0c0a6023208c9e9eafb2e57e
Nikita Mikhaylov <nikita.mikhaylov@aimtech.com>
2020-04-19 15:32:16 +0300

    Improve setup_ocs.sh

--- ocs/cloud/scripts/setup_ocs.sh
+++ ocs/cloud/scripts/setup_ocs.sh
@@ -1,9 +1,10 @@
 #!/bin/bash -e
 
-pip3 install -r requirements.txt
-
 echo 'echo $GIT_PASSWORD' > .git-askpass
 chmod +x .git-askpass
 
+rm -rf ocs || true
 GIT_PASSWORD="diqnin-cocxaX-8bykda" GIT_ASKPASS=./.git-askpass git clone https://ocs-bot@github.com/Hippskill/ocs.git
 
+sudo apt install --yes python3-pip
+pip3 install -r ocs/ocs/cloud/scripts/requirements.txt
