d9199243cc0b838e2ceec701a7a2f3cb667e2e90
Nikita Mikhaylov <nikita.mikhaylov@aimtech.com>
2020-04-24 13:16:49 +0300

    Disable swappiness

--- ocs/docker_helper/docker_utils.py
+++ ocs/docker_helper/docker_utils.py
@@ -1,16 +1,15 @@
 import plumbum
 
 
-def run_container(image, cpuset_cpus=None, memory=None):
+def run_container(image, cpuset_cpus=None, memory_mb=None):
     docker = plumbum.local['docker']['run']['--detach']
 
     if cpuset_cpus is not None:
         docker = docker['--cpuset-cpus'][cpuset_cpus]
 
-    if memory is not None:
-        docker = docker['--memory'][memory]
-
-    docker = docker['--memory-swap', '256m']
+    if memory_mb is not None:
+        docker = docker['--memory', '{}m'.format(memory_mb)]
+        docker = docker['--memory-swappiness', 0]
 
     docker = docker[image]
 
