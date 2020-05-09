1b305555348feadab091dcec41a4b7d650c04c68
Nikita Mikhaylov <nikita.mikhaylov@aimtech.com>
2020-05-09 12:32:10 +0300

    Add coordinate weights

--- ocs/algorithms/scout.py
+++ ocs/algorithms/scout.py
@@ -17,6 +17,17 @@ class Scout(Algorithm):
         self.max_runs_per_iter = config['max_runs_per_iter']
         self.cost_adjust_coef = config['cost_adjust_coef']
 
+        self.gamma = config['gamma']
+
+        # TODO(nmikhaylov): tweak weights by config
+        self.coordinate_weight = {
+            coordinate: 1.0 for coordinate in Instance.coordinates()
+        }
+
+        self.learning_rate = {
+            coordinate: 1.0 for coordinate in Instance.coordinates()
+        }
+
     def choose_best_instance(self, workload, env):
         random.seed(self.seed)
 
@@ -61,7 +72,8 @@ class Scout(Algorithm):
         diff_sum = 0.0
         for coordinate in Instance.coordinates():
             # TODO(nmikhaylov): add weight for coordinate
-            diff_sum += getattr(candidate, coordinate) - getattr(best_instance, coordinate)
+            coordinate_diff = getattr(candidate, coordinate) - getattr(best_instance, coordinate)
+            diff_sum += self.coordinate_weight[coordinate] * coordinate_diff
 
         weighted_diff = diff_sum * cost_diff
 
@@ -78,4 +90,27 @@ class Scout(Algorithm):
                 self.runs_per_instance
             ))
 
+            self._adjust_coordinate_weights(
+                best_instance=instances_with_run_results[0],
+                candidate_instance=instances_with_run_results[-1]
+            )
+
         return self.policy.choose_best_instance(instances_with_run_results)
+
+    def _adjust_coordinate_weights(self, best_instance, candidate_instance):
+        time_diff = candidate_instance.mean_elapsed - best_instance.mean_elapsed
+        cost_diff = candidate_instance.mean_cost - best_instance.mean_cost
+        for coordinate in Instance.coordinates():
+            coordinate_diff = getattr(candidate_instance.instance, coordinate) - getattr(best_instance.instance, coordinate)
+
+            gradient = -coordinate_diff * time_diff * cost_diff
+
+            print('time_diff: {} cost_diff: {} coordinate_diff: {} gradient: {}'.format(
+                time_diff,
+                cost_diff,
+                coordinate_diff,
+                gradient
+            ))
+
+            self.coordinate_weight[coordinate] += gradient * self.learning_rate[coordinate]
+            self.learning_rate[coordinate] *= self.gamma
